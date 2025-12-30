a1401a2e713ea4b40255d7f8da9b8906ffc7b7cc
angheloamir <angheloamir@gmail.com>
2025-12-31 01:27:47 +0800

    added policy to allow localhost:3000 to access the app

--- backend/node-service/src/config/authConfig.ts
+++ backend/node-service/src/config/authConfig.ts
@@ -2,5 +2,6 @@
 export const isAllowedOutsideCookies = true;
 export const allowedOrigins = [
     "http://127.0.0.1:5500",
-    "http://localhost:5199"
+    "http://localhost:5199",
+    "http://localhost:3000"
 ];
